3bc92597cb4cf0fdfe7c53425ecd71b21d890b23
macurrent <bruceminanga@gmail.com>
2023-05-15 23:09:13 +0300

    first commit

---
+++ articles/admin.py
@@ -0,0 +1,12 @@
+# articles/admin.py
+from django.contrib import admin
+
+from .models import Article
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ("title", "body",)
+    prepopulated_fields = {"slug": ("title",)}
+
+
+admin.site.register(Article, ArticleAdmin)
